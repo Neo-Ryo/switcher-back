315e0fd3f16ba695bdc4b368a449f0cfe0089b87
Neo-Ryo <marco.sch4064@gmail.com>
2020-07-12 18:46:10 +0200

    test

--- sequelize.js
+++ sequelize.js
@@ -10,17 +10,17 @@ const {
   DB_HOST,
 } = process.env;
 
-if (process.env.DATABASE_URL) {
-  module.exports = new Sequelize(process.env.DATABASE_URL, {
-    dialect: "postgres",
-  });
-} else {
-  module.exports = new Sequelize({
-    host: DB_HOST,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: NODE_ENV !== "test" ? DB_DATABASE : DB_TEST,
-    dialect: DB_DIALECT,
-    logging: false,
-  });
-}
+// if (process.env.DATABASE_URL) {
+//   module.exports = new Sequelize(process.env.DATABASE_URL, {
+//     dialect: "postgres",
+//   });
+// } else {
+module.exports = new Sequelize({
+  host: DB_HOST,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: NODE_ENV !== "test" ? DB_DATABASE : DB_TEST,
+  dialect: DB_DIALECT,
+  logging: false,
+});
+// }
